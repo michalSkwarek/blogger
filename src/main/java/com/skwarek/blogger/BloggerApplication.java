3951c626d4d023a68f901b8c1d9dbbead338d467
Michal <skwarek.michal.adrian@gmail.com>
2023-05-26 15:02:15 +0200

    Add a frame project

--- src/main/java/com/skwarek/blogger/BloggerApplication.java
+++ src/main/java/com/skwarek/blogger/BloggerApplication.java
@@ -2,12 +2,20 @@ package com.skwarek.blogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BloggerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BloggerApplication.class, args);
+    }
+
+    @GetMapping(value = {"/", "hello"})
+    public String sayHello() {
+        return "Hello you";
     }
 
 }
