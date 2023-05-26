f1101e6585d964071b776b84bbd1e1dfc49d80b6
Michal <skwarek.michal.adrian@gmail.com>
2023-05-26 21:13:15 +0200

    Add some find methods to repositories

--- src/main/java/com/skwarek/blogger/repository/CommentRepository.java
+++ src/main/java/com/skwarek/blogger/repository/CommentRepository.java
@@ -3,6 +3,10 @@ package com.skwarek.blogger.repository;
 import com.skwarek.blogger.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPostId(Long postId);
 
 }
--- src/main/java/com/skwarek/blogger/repository/PostRepository.java
+++ src/main/java/com/skwarek/blogger/repository/PostRepository.java
@@ -3,6 +3,10 @@ package com.skwarek.blogger.repository;
 import com.skwarek.blogger.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUserId(Long userId);
 
 }
